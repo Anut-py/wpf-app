57b034f57c0764294943fa4b6b476eabd47624be
Anand Swaroop <pritirekha@gmail.com>
2020-10-16 19:34:26 -0400

    Made backed connector and simple GUI

---
+++ MyFirstWpfApp/BackendConnector.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyFirstWpfApp
+{
+    public enum MovementType
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    }
+
+    public class BackendConnector
+    {
+        private const string LOCAL_URL = "http://localhost:9876";
+        private const string REMOTE_URL = "";
+        private const string BACKEND_URL = LOCAL_URL;
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<bool> move(int degrees, MovementType type)
+        {
+            var values = new Dictionary<string, string>
+            {
+                {"degrees", degrees.ToString()},
+                {"type", type.ToString()}
+            };
+            var content = new FormUrlEncodedContent(values);
+            return (await client.PostAsync($"{BACKEND_URL}/MoveWheels", content)).IsSuccessStatusCode;
+        }
+
+        public static async Task<bool> blink()
+        {
+            return (await client.GetAsync($"{BACKEND_URL}/Blink")).IsSuccessStatusCode;
+        }
+    }
+}
